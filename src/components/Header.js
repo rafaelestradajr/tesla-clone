d8910473eb861da1c243c5f6e8f7a77d83713040
Rafael Estrada <rafaelestrada@comcast.net>
2022-06-27 11:17:14 -0700

    Addes Redux store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,56 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Fade } from "react-awesome-reveal";
 import CloseIcon from '@mui/icons-material/Close';
+import {selectCars} from '../features/car/carSlice';
+import {useSelector} from 'react-redux';
 
 function Header() {
+const [burgerStatus, setBurgerStatus] =useState(false)
+const cars = useSelector(selectCars)
+
+
   return (
     
     <Container>
       <a>
-        <img src='/images/logo.svg' alt=''/>
+        <img  src='/images/logo.svg' alt=''/>
       </a>
 
       <Menu>
-        <a href='#'>Model S</a>
-        <a href='#'>Model 3</a>
-        <a href='#'>Model X</a>
-        <a href='#'>Model Y</a>
+        {cars && cars.map((car,index) => (
+        
+        <a key={index} href='#'>{car}</a>
+        ))}
+       
 
 
       </Menu>
 <RightMenu>
-  <a href='#'>Shop</a>
-  <a href='#'>Telsa Account</a>
-  <CustomMenu/>
+  <a href='https://shop.tesla.com/'>Shop</a>
+  <a href='https://auth.tesla.com/oauth2/v1/authorize?redirect_uri=https%3A%2F%2Fwww.tesla.com%2Fteslaaccount%2Fowner-xp%2Fauth%2Fcallback&response_type=code&client_id=ownership&scope=offline_access%20openid%20ou_code%20email&audience=https%3A%2F%2Fownership.tesla.com%2F&locale=en-US'>Telsa Account</a>
+  <CustomMenu onClick={() => setBurgerStatus(true)}/>
   
 </RightMenu>
-<BurgerNav>
-  <CloseWrapper>
+<BurgerNav show={burgerStatus} >
+  <CloseWrapper onClick={() => setBurgerStatus(false)}>
 
   <CustomClose/>
   </CloseWrapper>
+  
 
-  <li><a href='#'>Existing Inventory</a></li>
-  <li><a href='#'>Used Inventory</a></li>
-  <li><a href='#'>Trade-In Inventory</a></li>
-  <li><a href='#'>Cybertruck Inventory</a></li>
-  <li><a href='#'>Roadster Inventory</a></li>
-  <li><a href='#'>Existing Inventory</a></li>
-  <li><a href='#'>Existing Inventory</a></li>
-  <li><a href='#'>Existing Inventory</a></li>
+  <li><a href='https://www.tesla.com/models'>Model - S</a></li>
+  <li><a href='https://www.tesla.com/model3'>Model - 3</a></li>
+  <li><a href='https://www.tesla.com/modelx'>Model - X</a></li>
+  <li><a href='https://www.tesla.com/modely'>Model - Y</a></li>
+
+  
+  <li><a href='https://www.tesla.com/inventory/new/m3?arrangeby=relevance&zip=95621&range=200'>Existing Inventory</a></li>
+  <li><a href='https://www.tesla.com/inventory/used/m3?arrangeby=plh&zip=95621'>Used Inventory</a></li>
+  <li><a href='https://www.tesla.com/tradein'>Trade-In Inventory</a></li>
+  <li><a href='https://www.tesla.com/cybertruck'>Cybertruck Inventory</a></li>
+  <li><a href='https://www.tesla.com/roadster'>Roadster Inventory</a></li>
 </BurgerNav>
 
     </Container>
@@ -58,7 +69,7 @@ const Container = styled.div`
   top:0 ;
   left:0;
   right:0;
-
+  z-index:1;
 `
 
 const Menu = styled.div`
@@ -109,6 +120,9 @@ padding:20px;
 display:flex;
 text-align:start;
 flex-direction:column;
+transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+transition: transform 0.2s;
+
 li{
   padding:15px 0;
   border-bottom:1px solid rgba(0,0,0, .2);
